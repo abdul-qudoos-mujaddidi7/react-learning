15357b653b7aefe544d56083170d171192b0ea2a
Jawad Mujaddidi <abdulqudoosmujaddidi7@gmail.com>
2026-01-15 13:09:31 +0430

    Initial React setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { use } from "react";
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  return null
+}
+
+export default App;
