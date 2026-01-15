21929e6319934044b3b6a70d3e506272226bb9b1
Jawad Mujaddidi <abdulqudoosmujaddidi7@gmail.com>
2026-01-15 17:13:14 +0430

    Creating a State Variable With useState

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
 
   const [step, setStep] = useState(1);
 
-function previous() {
+function handlePrevious() {
   if (step >1){
     setStep(step - 1);
   }
 }
 
-function next() {
+function handleNext() {
   if (step < messages.length){
     setStep(step + 1);
   }
@@ -30,8 +30,8 @@ function next() {
     </div>
     <p className="message">Step {step}: {messages[step - 1]}</p>
     <div className="buttons">
-      <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={previous}>Previous</button>
-      <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={next}>Next</button>
+      <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handlePrevious}>Previous</button>
+      <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handleNext}>Next</button>
       </div>
 
   </div>
