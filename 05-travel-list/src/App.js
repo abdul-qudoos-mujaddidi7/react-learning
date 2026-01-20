f7f28245cf6c31f7feab4e01f07282e03ddb65e3
Jawad Mujaddidi <abdulqudoosmujaddidi7@gmail.com>
2026-01-20 19:15:58 +0430

    Building the Static Layout of Travel List mini Project

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,7 +1,36 @@
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
+}
+
+
+function Logo() {
+  return <h1>🌴 Far Away Travel 🧳</h1>;
+}
+
+function Form() {
+  return  <div className="add-form">
+    <h3>What do u need for your 😊 trip?</h3>
+  </div>
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      List
+    </div>
+  );
+ 
+}
+
+function Stats() {
+  return <footer className="stats">
+    <em>You have x item on your list, and you already picked X(X%) </em></footer>;
+ 
 }
