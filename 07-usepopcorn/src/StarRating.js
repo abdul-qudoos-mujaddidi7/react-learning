3b5ccc744aed5ef2d08bc1107c3c916ffd9e8592
Jawad Mujaddidi <abdulqudoosmujaddidi7@gmail.com>
2026-02-13 00:08:33 +0430

    Handling Over Event

--- 07-usepopcorn/src/StarRating.js
+++ 07-usepopcorn/src/StarRating.js
@@ -18,6 +18,7 @@ const textStyle = {
 
 export default function StarRating({maxRating =5}) {
     const [rating, setRating]=useState(0)
+    const [temp, setTemRating]=useState(0)
      function handleRating(rating){
         setRating(rating)
     }
@@ -25,16 +26,16 @@ export default function StarRating({maxRating =5}) {
     <div style={containerStyle}>
       <div style={startContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-            <Star key={i}  onRate={()=>handleRating(i+1)} full={rating>=i+1}/>
+            <Star key={i}  onRate={()=>handleRating(i+1)} full={temp?temp>=i+1:rating>=i+1} onHoverIn={()=>setTemRating(i+1)} onHoverOut={()=>setTemRating(0)}/>
           
         ))}
       </div>
-      <div style={textStyle}>{rating ||""}</div>
+      <div style={textStyle}>{ temp|| rating ||""}</div>
     </div>
   );
 }
 
-function Star({onRate, full}){
-   
-    return <span role="button"  onClick={onRate}>{full ?"⭐":"😁"}</span>
+function Star({onRate, full, onHoverIn, onHoverOut}){
+    return <span role="button"  onClick={onRate} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>    
+  {full ?"⭐":"😁"}</span>
 }
